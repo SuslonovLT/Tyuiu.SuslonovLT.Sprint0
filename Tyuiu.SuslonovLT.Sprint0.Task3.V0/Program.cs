213461e127620972fc10ae97f00bc8853ecce342
SuslonovLT <suslonov08@gmail.com>
2026-09-13 19:33:55 +0500

    Add files via upload
    Task3

---
+++ Tyuiu.SuslonovLT.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SuslonovLT.Sprint0.Task3.V0.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
