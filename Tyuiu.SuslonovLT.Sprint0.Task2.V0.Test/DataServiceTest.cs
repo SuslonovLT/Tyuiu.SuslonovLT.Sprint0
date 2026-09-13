06b8ae52a9829beb6c34eb0f8f48091d5c3ef0c3
SuslonovLT <suslonov08@gmail.com>
2026-09-13 18:00:28 +0500

    Add files via upload
    Task2

---
+++ Tyuiu.SuslonovLT.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SuslonovLT.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SuslonovLT.Sprint0.Task2.V0.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Леонид";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Леонид", res);
+        }
+    }
+}
